bbe97fa9054aafe30f25863af5200d753d724f9e
unknown <jacob220@hotmail.fr>
2024-08-02 11:34:45 +0200

    rgrgrgr

--- struct_clio.c
+++ struct_clio.c
@@ -16,7 +16,7 @@ struct voiture v;
 strcpy(v.nom, "Clio5");
 v.km=90000;
 v.annee=2017;
-printf("   votre voiture est %s.elle a %d km ; elle est de l'annee %d",v.nom,v.km,v.annee);
+printf("      votre voiture est %s.elle a %d km ; elle est de l'annee %d",v.nom,v.km,v.annee);
 ///dzdsdssddsdsdsdsd
 return 0;
 }
