d85b07a4ec9c199a4a2b7e3e89564e027c2816ec
unknown <jacob220@hotmail.fr>
2024-08-02 11:06:39 +0200

    pour un conflit

--- struct_clio.c
+++ struct_clio.c
@@ -17,7 +17,7 @@ strcpy(v.nom, "Clio5");
 v.km=90000;
 v.annee=2017;
 printf("votre voiture est %s.elle a %d km ; elle est de l'annee %d",v.nom,v.km,v.annee);
-//BONJOUR
+///dzdsdssddsdsdsdsd
 return 0;
 }
 
